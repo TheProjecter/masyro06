f137e7e0783aba81756f14ea2f1c3134abe62672
DVallejo.Fernandez <DVallejo.Fernandez@c1553c09-691d-0410-a278-63ba1714d43f>
2006-09-14 13:28:13 +0000

    gui

---
+++ gui/js/initscript.js
@@ -0,0 +1,27 @@
+      // Para realizar el rollover de las imagenes 
+      function imageOn (el) {
+            var str_switch = "on";
+            chr = el.src.lastIndexOf("-") + 1;
+            period = el.src.lastIndexOf(".");
+
+            el.src = "" + el.src.substring(0, chr) + str_switch + el.src.substring(period, el.src.length);
+            return true;
+      }
+
+ 	   function imageClick (el) {
+            var str_switch = "click";
+            chr = el.src.lastIndexOf("-") + 1;
+            period = el.src.lastIndexOf(".");
+
+            el.src = "" + el.src.substring(0, chr) + str_switch + el.src.substring(period, el.src.length);
+            return true;
+      }
+      function imageOff (el) {
+            var str_switch = "off";
+            chr = el.src.lastIndexOf("-") + 1;
+            period = el.src.lastIndexOf(".");
+
+            el.src = "" + el.src.substring(0, chr) + str_switch + el.src.substring(period, el.src.length);
+            return true;
+      }
+      
